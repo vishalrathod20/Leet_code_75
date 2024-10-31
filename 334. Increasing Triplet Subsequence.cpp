5363d5849f30c3209f8d1d831b2445f5bce9cf36
Vishal Rathod <vishalrathod202004@gmail.com>
2024-10-31 09:21:05 +0530

    Create 334. Increasing Triplet Subsequence.cpp

---
+++ 334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX, second = INT_MAX;
+
+        for(int num : nums){
+            if(first>= num){
+                first = num;
+            }
+            else if (num<=second){
+                second = num;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
